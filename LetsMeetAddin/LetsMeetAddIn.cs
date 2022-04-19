ed75ec2286d1c9276c0ecf1281082c8eff5276a8
Redfoxbox <kerecsen.gergo@gmail.com>
2022-04-19 17:01:25 +0200

    init

---
+++ LetsMeetAddin/LetsMeetAddIn.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace LetsMeetAddin
+{
+    public partial class LetsMeetAddIn
+    {
+        Outlook.Inspectors inspectors;
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            inspectors = this.Application.Inspectors;
+            inspectors.NewInspector +=
+            new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
+        }
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Settings_ribbon();
+        }
+
+        void Inspectors_NewInspector(Microsoft.Office.Interop.Outlook.Inspector Inspector)
+        {
+
+            Outlook.AppointmentItem agendaMeeting = Inspector.CurrentItem as Outlook.AppointmentItem;
+
+            if (agendaMeeting != null)
+            {
+
+                if (agendaMeeting.Location is null && agendaMeeting.Subject is null && agendaMeeting.Body is null)
+                {
+                    string meetid = RandomString(20);
+                    agendaMeeting.MeetingStatus = Microsoft.Office.Interop.Outlook.OlMeetingStatus.olMeeting;
+                    agendaMeeting.Location = Properties.Settings.Default.MeetName + " --- " + Properties.Settings.Default.MeetLink + meetid;
+                    RichTextBox dynamicRichTextBox = new RichTextBox();
+                    dynamicRichTextBox.BackColor = Color.White;
+                    dynamicRichTextBox.Clear();
+                    dynamicRichTextBox.SelectionFont = new Font("Calibri", 11);
+                    dynamicRichTextBox.SelectedText = "\n\n";
+                    dynamicRichTextBox.BulletIndent = 0;
+                    dynamicRichTextBox.SelectionBullet = false;
+                    dynamicRichTextBox.SelectionFont = new Font("Arial", 16);
+                    dynamicRichTextBox.AppendText("___________________________\n\n");
+                    dynamicRichTextBox.SelectionFont = new Font("Arial", 16);
+                    dynamicRichTextBox.SelectedText = Properties.Settings.Default.MeetName + "\n\n";
+                    dynamicRichTextBox.SelectionFont = new Font("Arial", 12);
+                    dynamicRichTextBox.SelectedText = Properties.Settings.Default.MeetDesc + "\n\n";
+                    dynamicRichTextBox.SelectionFont = new Font("Arial", 12);
+                    dynamicRichTextBox.SelectedText = Properties.Settings.Default.MeetLink + meetid + "\n";
+                    agendaMeeting.RTFBody = System.Text.Encoding.UTF8.GetBytes(dynamicRichTextBox.Rtf);
+                }
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ LetsMeetAddin/Settings_ribbon.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new Settings_ribbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace LetsMeetAddin
+{
+    [ComVisible(true)]
+    public class Settings_ribbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Settings_ribbon()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("LetsMeetAddin.Settings_ribbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+        public string EditBoxGetTextLink(Office.IRibbonControl control)
+        {
+            String meetlink = Properties.Settings.Default.MeetLink;
+            return meetlink;
+        }
+
+        public void editBoxLink_TextChanged(Office.IRibbonControl control, String text)
+        {
+            Properties.Settings.Default.MeetLink = text;
+        }
+
+        public string EditBoxGetTextName(Office.IRibbonControl control)
+        {
+            String meetlink = Properties.Settings.Default.MeetName;
+            return meetlink;
+        }
+
+        public void editBoxName_TextChanged(Office.IRibbonControl control, String text)
+        {
+            Properties.Settings.Default.MeetName = text;
+        }
+
+        public string EditBoxGetTextDesc(Office.IRibbonControl control)
+        {
+            String meetlink = Properties.Settings.Default.MeetDesc;
+            return meetlink;
+        }
+
+        public void editBoxDesc_TextChanged(Office.IRibbonControl control, String text)
+        {
+            Properties.Settings.Default.MeetDesc = text;
+        }
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
